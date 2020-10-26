2b7d542d3f0a3d91259fcd7d051235a89e480d16
Jesus Molano <jessumolano@gmail.com>
2020-10-26 15:37:28 +0000

    Finish Objects

--- developedbyed/objects/objects.js
+++ developedbyed/objects/objects.js
@@ -71,3 +71,64 @@ User.prototype.getName = function () {
     console.log(this.name);
 };
 user1.getName();
+
+///////////////////////////////////////////////////
+//Constructor Enemy
+function Enemy(life, name, level) {
+    this.life = life;
+    this.name = name;
+    this.level = level;
+}
+Enemy.prototype.getInfo = function () {
+    console.log(this.life, this.name, this.level);
+};
+Enemy.prototype.attack = function () {
+    console.log(`${this.name} has attacked`);
+};
+Enemy.prototype.block = function () {
+    console.log(`${this.name} has blocked`);
+};
+
+//Constructor Dragon
+function Dragon(life, name, level, color, spell) {
+    //Keyword this is going reference the Dragon object
+    Enemy.call(this, life, name, level);
+    //Define it as normal
+    this.color = color;
+    this.spell = spell;
+}
+// INHERIT PROTOTYPE
+Dragon.prototype = Object.create(Enemy.prototype);
+Dragon.prototype.fireBreath = function () {
+    console.log("FireBreathhhh!!!");
+};
+const newDragon = new Dragon(100, "Drogon", 20, "black", "Drakaris");
+
+console.log(newDragon);
+newDragon.getInfo();
+
+/////////////////////////////////////////////////////////
+// CLASES
+class Ally {
+    constructor(life, name, level) {
+        this.life = life;
+        this.name = name;
+        this.level = level;
+    }
+    getInfo() {
+        console.log(this.life, this.name, this.level);
+    }
+}
+const ally = new Ally(100, "Dante", 25);
+console.log(ally);
+
+class Pet extends Ally {
+    constructor(life, name, level, legs, dmg) {
+        super(life, name, level);
+        this.legs = legs;
+        this.dmg = dmg;
+    }
+}
+const pet = new Pet(100, "Alay", 10, 4, 300);
+console.log(pet);
+pet.getInfo();
